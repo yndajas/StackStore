5b2be9306855c230abdddec7a350fee146a4684c
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-22 04:56:02 +0100

    add question tag table/model/controller

---
+++ backend/db/migrate/20210822031704_create_question_tags.rb
@@ -0,0 +1,10 @@
+class CreateQuestionTags < ActiveRecord::Migration[6.1]
+  def change
+    create_table :question_tags do |t|
+      t.references :question, null: false, foreign_key: true
+      t.references :tag, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
