321b6989c3b1213ab2d1fdfd6fe134fb45bad103
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-22 19:42:58 +0100

    add secure password requirement for users

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    has_many :answers
-    has_many :questions
-    has_many :question_tags, through: :questions
+  has_many :answers
+  has_many :questions
+  has_many :question_tags, through: :questions
+  has_secure_password
 end
