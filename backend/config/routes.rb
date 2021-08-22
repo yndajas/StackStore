eb962fe7ec27fad4e61a2c5b17700151dc0a593c
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-22 04:56:30 +0100

    add placeholder resource routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :question_tags
+  resources :tags
+  resources :answers
+  resources :questions
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
