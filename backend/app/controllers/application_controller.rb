5668557a152bf1e5a796aeeeb8c04e90676ae403
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-22 19:42:19 +0100

    add authorisation check for before action use

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  def render_error_if_user_mismatch
+    if params[:id]
+      user = User.find(params[:id])
+      render json: { error: 'access denied' } if !user || !request.headers['token'].try('==', user.token)
+    end
+  end
 end
