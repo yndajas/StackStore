12cceff842d93293bb139dedfb982b82d10e06d2
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-22 19:42:39 +0100

    add create method to users controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def create
+    if User.find_by(email: params[:email])
+      render json: { error: 'User already exists', status: 401 }
+    else
+      user = User.create(email: params[:email], password: params[:password], token: SecureRandom.urlsafe_base64)
+      render json: { user: { id: user.id, email: user.email, token: user.token } }
+    end
+  end
 end
