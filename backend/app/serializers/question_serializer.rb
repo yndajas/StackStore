9495ed2f2fc4090cdc1a50b3e2cd7e1d66e02d28
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-23 21:31:57 +0100

    add answer/question tag relationships to question serializer

--- backend/app/serializers/question_serializer.rb
+++ backend/app/serializers/question_serializer.rb
@@ -1,4 +1,6 @@
 class QuestionSerializer
   include JSONAPI::Serializer
+  has_many :answers
+  has_many :question_tags
   attributes :user_id, :stack_id, :score, :title, :body, :stack_created, :stack_updated, :notes
 end
