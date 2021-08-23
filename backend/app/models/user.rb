f7e8c5544f9ab5b62af9d425f3a9c59638df9e62
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-23 21:17:20 +0100

    add user email uniqueness validation

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :question_tags, through: :questions
   has_secure_password
 
+  validates :email, uniqueness: true
+
   def questions
     Question.find_by(user_id: id)
   end
