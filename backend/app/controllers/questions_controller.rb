88e82ab3b788f9c2a1849ffde9d020df692b7056
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-23 21:19:40 +0100

    add show/create/update actions to questions controller
    - also fix error in index action

--- backend/app/controllers/questions_controller.rb
+++ backend/app/controllers/questions_controller.rb
@@ -2,43 +2,36 @@ class QuestionsController < ApplicationController
   before_action :render_error_if_user_mismatch
 
   def index
-    user = params[:user_id]
+    user = User.find(params[:user_id])
     questions = user.questions
     render json: QuestionSerializer.new(questions)
   end
 
   def show
-    # get 'users/:user_id/questions/:question_id' => 'questions#show'
+    question = Question.find_by(user_id: params[:user_id], id: params[:question_id])
+    if question
+      render json: QuestionSerializer.new(question)
+    else
+      render json: { error: "Question with ID #{params[:question_id]} not found for current user" }, status: 404
+    end
   end
 
   def create
-    # post 'users/:user_id/questions' => 'questions#create'
-    # Question.create(attribute: params[:attribute])
+    question = Question.find_or_initialize_by(user_id: params[:user_id], stack_id: params[:stack_id])
+    new_question = question.new_record?
+    question.update_from_params(params) if new_question
 
-    # old code - take the find or initialise by (stack question ID) and new record?, then if new record add the remaining attributes and save, then render json of the persisted instance
-
-    # UserJoke.approve_or_reject(params[:id], params[:identifier], params[:approveOrReject], params[:additional_attributes])
-
-    # def approve_or_reject(user_id, identifier, approve_or_reject, additional_attributes = nil)
-    #     approved = (approve_or_reject == 'approve') ? true : false
-    #     resource_model = self.name.gsub("User", "").constantize
-    #     resource_instance = resource_model.find_or_initialize_by(:"#{resource_model.identifier}" => identifier)
-
-    #     if resource_instance.new_record?
-    #         if additional_attributes
-    #             additional_attributes.each do |key, value|
-    #                 resource_instance.send("#{key}=", value)
-    #             end
-    #         end
-    #         resource_instance.save
-    #     end
-
-    #     user_resource_instance = self.find_or_create_by(user_id: user_id, :"#{self.name.gsub("User", "").underscore}_id" => resource_instance.id)
-    #     user_resource_instance.update(approved: approved)
-    # end
+    serialized_question_hash = QuestionSerializer.new(question).serializable_hash
+    serialized_question_hash['new'] = new_question
+    render json: serialized_question_hash.to_json
   end
 
   def update
-    # patch 'users/:user_id/questions/:question_id' => 'questions#update'
+    question = Question.find(params[:question_id])
+    question.update_from_params(params)
+
+    render json: QuestionSerializer.new(question)
   end
+
+  # add delete?
 end
