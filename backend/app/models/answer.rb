4d882925482b3eee24ff3e1eeda67f37851c3658
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-24 19:33:30 +0100

    add method for creating/updating answers

--- backend/app/models/answer.rb
+++ backend/app/models/answer.rb
@@ -5,4 +5,13 @@ class Answer < ApplicationRecord
   validates :user_id, uniqueness: { scope: :stack_id }
 
   # validate for presence/length of attributes
+
+  def self.create_or_update_from_hash_and_question(hash, question)
+    answer = find_or_initialize_by(user_id: question.user_id, stack_id: hash[:stack_id])
+    answer.question = question
+    %w[stack_question_id accepted score body stack_created stack_updated notes].each do |attribute|
+      answer.send("#{attribute}=", hash[:"#{attribute}"])
+    end
+    save
+  end
 end
