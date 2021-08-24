9ea6daee6bd55541cccad48ed19f8f7bc2d963f8
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-24 18:07:04 +0100

    add index and show actions to tag controller

--- backend/app/controllers/tags_controller.rb
+++ backend/app/controllers/tags_controller.rb
@@ -1,2 +1,16 @@
 class TagsController < ApplicationController
+  def index
+    user = User.find(params[:user_id])
+    tags = user.tags
+
+    render json: TagSerializer.new(tags)
+  end
+
+  def show
+    user = User.find(params[:user_id])
+    tag = Tag.find_by_slug(params[:tag_slug])
+    questions = user.questions_by_tag(tag)
+
+    render json: QuestionSerializer.new(questions)
+  end
 end
