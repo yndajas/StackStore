9f5ddd454350f4a06d7f041f6f287323dac27f1b
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-24 18:06:21 +0100

    add has many through relationships to questions and tags

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers
   has_many :question_tags
+  has_many :tags, through: :question_tags
 
   validates :user_id, uniqueness: { scope: :stack_id }
 
--- backend/app/models/tag.rb
+++ backend/app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
   has_many :question_tags
+  has_many :questions, through: :question_tags
 
   validates :text, uniqueness: true
 
