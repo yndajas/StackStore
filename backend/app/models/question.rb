13fe7e8b413229e731091eda2905c5fee24d608f
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-24 19:34:34 +0100

    add answer management to question method

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -8,10 +8,17 @@ class Question < ApplicationRecord
 
   # validate for presence/length of attributes
 
-  def update_from_params(params)
+  def add_or_update_attributes_from_params(params)
     %w[score title body stack_created stack_updated notes].each do |attribute|
       send("#{attribute}=", params[:"#{attribute}"])
     end
     save
+
+    # add/update answers
+    params[:answers].each { |answer_hash| Answer.create_or_update_from_hash_and_question(answer_hash, self) }
+
+    # remove answers no longer present in source
+    answers_stack_ids = params[:answers].collect(&:stack_id)
+    answers.each { |answer| answer.destroy unless answers_stack_ids.include?(answer.stack_id) }
   end
 end
