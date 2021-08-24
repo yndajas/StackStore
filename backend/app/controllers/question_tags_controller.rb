1f7bada028925eaece2f88be98b9db57a395a142
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-24 01:45:08 +0100

    re-add placeholder question tags controller

---
+++ backend/app/controllers/question_tags_controller.rb
@@ -0,0 +1,4 @@
+class QuestionTagsController < ApplicationController
+  # post 'users/:user_id/question-tags' => 'question_tags#create'
+  # delete 'users/:user_id/question-tags/:question_tag_id' => 'question_tags#destroy'
+end
