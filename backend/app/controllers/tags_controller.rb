761c7ec7bfd13838e985080816671de50ea14ca9
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-25 19:16:17 +0100

    add record not found check

--- backend/app/controllers/tags_controller.rb
+++ backend/app/controllers/tags_controller.rb
@@ -9,6 +9,7 @@ class TagsController < ApplicationController
   def show
     user = User.find(params[:user_id])
     tag = Tag.find_by_slug(params[:tag_slug])
+    render_error_if_record_not_found(tag)
     questions = user.questions_by_tag(tag)
 
     render json: QuestionSerializer.new(questions)
