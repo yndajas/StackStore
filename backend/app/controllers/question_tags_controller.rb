cf587314e4978da64db7db81247a815053a6632e
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-25 19:16:48 +0100

    add question tag create/destroy actions in controller

--- backend/app/controllers/question_tags_controller.rb
+++ backend/app/controllers/question_tags_controller.rb
@@ -1,4 +1,23 @@
 class QuestionTagsController < ApplicationController
-  # post 'users/:user_id/question-tags' => 'question_tags#create'
-  # delete 'users/:user_id/question-tags/:question_tag_id' => 'question_tags#destroy'
+  def create
+    tag = Tag.find_or_create_by(text: params[:text].lower)
+    question_tag = Tag.find_or_initialize_by(question_id: params[:question_id], tag_id: tag.id)
+    new_question_tag = question_tag.new_record?
+
+    # add new flag to JSON
+    serialized_question_tag_hash = QuestionTagSerializer.new(question_tag).serializable_hash
+    serialized_question_tag_hash['new'] = new_question_tag
+
+    render json: serialized_question_tag_hash.to_json
+  end
+
+  def destroy
+    question_tag = QuestionTag.find(params[:question_tag_id])
+    render_error_if_record_not_found(question_tag)
+    tag = question_tag.tag
+    question_tag.destroy
+    tag.destroy if tag.question_tags.length == 0
+
+    render json: { error: 'Tag successfully removed' }, status: 200
+  end
 end
