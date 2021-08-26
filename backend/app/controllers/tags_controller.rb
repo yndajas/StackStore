36b2aed9076551d119c93f2bd2c519d8f71298b6
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-08-26 01:03:44 +0100

    check user

--- backend/app/controllers/tags_controller.rb
+++ backend/app/controllers/tags_controller.rb
@@ -1,16 +1,16 @@
 class TagsController < ApplicationController
+  before_action :check_and_set_user
+
   def index
-    user = User.find(params[:user_id])
-    tags = user.tags
+    tags = @user.tags
 
     render json: TagSerializer.new(tags)
   end
 
   def show
-    user = User.find(params[:user_id])
     tag = Tag.find_by_slug(params[:tag_slug])
     render_error_if_record_not_found(tag)
-    questions = user.questions_by_tag(tag)
+    questions = @user.questions_by_tag(tag)
 
     render json: QuestionSerializer.new(questions)
   end
