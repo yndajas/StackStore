94a39adf0144bc0406ec4d4c7f72d85330752731
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-12 21:50:33 +0100

    add footer container

---
+++ src/containers/FooterContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class HeaderContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Logged in account placeholder</p>
+        <p>Copyright placeholder</p>
+      </div>
+    );
+  }
+}
+
+export default HeaderContainer;
