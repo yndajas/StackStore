4bccf34dc9c3ea61c15dbe8625f2dcdd6f6ada88
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-16 02:55:19 +0100

    add link to logo; shift Router; add class to links

--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -11,41 +11,49 @@ class HeaderContainer extends React.Component {
       <header>
         <nav className="navbar navbar-expand-md navbar-light">
           <div className="container">
-            <img
-              src={process.env.PUBLIC_URL + "/images/logo.png"}
-              className="logo"
-              alt="logo"
-              title="StackStore logo"
-            />
-            <button
-              aria-controls="navbar"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-              className="navbar-toggler"
-              data-target="#navbar"
-              data-toggle="collapse"
-              type="button"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbar">
-              <ul className="navbar-nav mr-auto mt-2" id="nav-elements">
-                <Router>
+            <Router>
+              <Link to="/">
+                <img
+                  src={process.env.PUBLIC_URL + "/images/logo.png"}
+                  className="logo"
+                  alt="logo"
+                  title="StackStore logo"
+                />
+              </Link>
+              <button
+                aria-controls="navbar"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                className="navbar-toggler"
+                data-target="#navbar"
+                data-toggle="collapse"
+                type="button"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbar">
+                <ul className="navbar-nav mr-auto mt-2" id="nav-elements">
                   <li className="nav-item">
-                    <Link to="/questions">Questions</Link>
+                    <Link to="/questions" className="nav-link">
+                      Questions
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/tags">Tags</Link>
+                    <Link to="/tags" className="nav-link">
+                      Tags
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/search">Search</Link>
+                    <Link to="/search" className="nav-link">
+                      Search
+                    </Link>
                   </li>
-                </Router>
-              </ul>
-              <ul className="navbar-nav ml-auto mt-2" id="session-control">
-                {this.props.user ? <LogOutButton /> : <SessionInput />}
-              </ul>
-            </div>
+                </ul>
+                <ul className="navbar-nav ml-auto mt-2" id="session-control">
+                  {this.props.user ? <LogOutButton /> : <SessionInput />}
+                </ul>
+              </div>
+            </Router>
           </div>
         </nav>
       </header>
