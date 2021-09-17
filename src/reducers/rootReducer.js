2316ca82d3642c1e959914456c14684876cd6b74
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-17 03:42:34 +0100

    add question and search actions to reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,8 +3,22 @@ export default function rootReducer(state = {}, action) {
     case "LOG_IN":
       return { ...state, user: action.payload };
     case "LOG_OUT":
+      return {};
+    case "FETCH_SAVED_QUESTIONS":
+      return { ...state, questions: action.payload };
+    case "SET_SEARCH_QUERY":
+      return { ...state, searchQuery: action.payload };
+    case "SEARCH_QUESTIONS":
+      return {
+        ...state,
+        searchResults: action.payload.items,
+        searchResultsTotal: action.payload.total,
+      };
+    case "CLEAR_SEARCH":
       const newState = { ...state };
-      delete newState.user;
+      delete newState.searchQuery;
+      delete newState.searchResults;
+      delete newState.searchResultsTotal;
       return newState;
     default:
       return state;
