51afae36dbdc69dd7098b571799ecd1d57493117
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-17 03:38:25 +0100

    add search results and saved questions card components

---
+++ src/components/savedQuestions/SavedQuestionCard.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import QuestionData from "../QuestionData";
+import NotesContainer from "../../containers/main/NotesContainer";
+import TagsContainer from "../../containers/main/TagsContainer";
+import AnswerData from "../AnswerData";
+
+class SavedQuestionCard extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card-header" id={`heading${this.props.question.id}`}>
+          <button
+            className="btn"
+            data-toggle="collapse"
+            data-target={`#collapse${this.props.question.id}`}
+            aria-expanded="false"
+            aria-controls={`collapse${this.props.question.id}`}
+          >
+            <h3
+              dangerouslySetInnerHTML={{
+                __html: this.props.question.title,
+              }}
+            />
+          </button>
+        </div>
+        <div
+          className="collapse"
+          id={`collapse${this.props.question.id}`}
+          aria-labelledby={`heading${this.props.question.id}`}
+          data-parent="#accordion"
+        >
+          <div className="card-body">
+            <QuestionData question={this.props.question} />
+            {/* route/switch/if for notes: add button/show with edit button/edit form */}
+            <NotesContainer question={this.props.question} />
+            {/* route/switch/if for tags: add button/show with remove button or text/edit with comma-separated tags and save button */}
+            <TagsContainer question={this.props.question} />
+            <hr />
+            {this.props.question.answers.map((answer) => (
+              <AnswerData answer={answer} />
+              //   answer notes container
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SavedQuestionCard;
