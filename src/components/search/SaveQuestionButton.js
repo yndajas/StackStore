021b8a7b806e110ecbb3151494a0302279467311
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-19 00:18:57 +0100

    add save question button

---
+++ src/components/search/SaveQuestionButton.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { saveQuestion } from "../../actions/saveQuestion";
+
+class SaveQuestionButton extends React.Component {
+  render() {
+    const checkIfSavedOrUpdated = () => {
+      if (this.props.savedQuestions) {
+        let savedQuestion = this.props.savedQuestions.find(
+          (savedQuestion) =>
+            savedQuestion.data.attributes.stack_id === this.props.question.id
+        );
+
+        if (savedQuestion) {
+          if (savedQuestion.new) {
+            return "Saved";
+          } else {
+            return "Updated";
+          }
+        }
+      } else {
+        return false;
+      }
+    };
+
+    return (
+      <button
+        disabled={!!checkIfSavedOrUpdated()}
+        name="save-question"
+        className="btn btn-success"
+        onClick={() =>
+          this.props.saveQuestion(this.props.user, this.props.question)
+        }
+      >
+        {checkIfSavedOrUpdated() || "Save"}
+      </button>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    savedQuestions: state.savedQuestions,
+  };
+};
+
+export default connect(mapStateToProps, { saveQuestion })(SaveQuestionButton);
