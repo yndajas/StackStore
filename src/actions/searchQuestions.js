8dc6583e4121bca1ee132750ae049e828ebd8f20
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-18 20:49:12 +0100

    sort question and answer data after fetching from external API

--- src/actions/searchQuestions.js
+++ src/actions/searchQuestions.js
@@ -2,9 +2,35 @@ export const searchQuestions = (query) => {
   return (dispatch) => {
     dispatch({ type: "SET_SEARCH_QUERY", payload: query });
     fetch(
-      `https://api.stackexchange.com/2.3/search?page=1&pagesize=20&order=desc&sort=activity&intitle=${query}&site=stackoverflow&filter=!*IUCsILPFff.gUR-)krzE*H5D6W-09LURZPSIDyhZgVw)SBYNUyc5Vx16)yTJH`
+      `https://api.stackexchange.com/2.3/search?page=1&pagesize=20&order=desc&sort=activity&intitle=${query}&site=stackoverflow&filter=!SBqKECKMLIB-ijOvJS9H1*qxQqh7Evi_6BCJ0xoAl1DWJn*5.(Ks-p).sR*s9M_*`
     )
       .then((response) => response.json())
-      .then((json) => dispatch({ type: "SEARCH_QUESTIONS", payload: json }));
+      .then((json) => {
+        const sortedItems = json.items
+          .sort((a, b) => parseInt(b.answer_count) - parseInt(a.answer_count))
+          .sort((a, b) => parseInt(b.score) - parseInt(a.score))
+          .map((sortedItem) => {
+            if (sortedItem.answers) {
+              const sortedAnswers = sortedItem.answers.sort((a, b) => {
+                if (a.is_accepted) {
+                  return -1;
+                } else if (b.is_accepted) {
+                  return 1;
+                } else {
+                  return parseInt(b.score) - parseInt(a.score);
+                }
+              });
+
+              return { ...sortedItem, answers: sortedAnswers };
+            } else {
+              return sortedItem;
+            }
+          });
+
+        dispatch({
+          type: "SEARCH_QUESTIONS",
+          payload: { ...json, items: sortedItems },
+        });
+      });
   };
 };
