bdd827b9e9236b9d8ae3df1c1ce263164b8620cb
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-19 00:19:15 +0100

    add save question button to search result card

--- src/components/search/SearchResultCardBody.js
+++ src/components/search/SearchResultCardBody.js
@@ -1,20 +1,23 @@
 import QuestionData from "../QuestionData";
 import AnswerData from "../AnswerData";
+import SaveQuestionButton from "./SaveQuestionButton";
 
 const SearchResultCardBody = ({ question }) => {
   return (
     <div className="card-body">
       <QuestionData question={question} />
       <h3>Answers</h3>
-      {question.answers
-        ? question.answers.map((answer, index) => (
-            <>
-              <AnswerData answer={answer} />
-              {index < question.answers.length - 1 ? <hr /> : ""}
-            </>
-          ))
-        : "No answers"}
-      {/* button to save question (use Stack question_id and package the data from the search results state object corresponding to that ID) */}
+      {question.answers ? (
+        question.answers.map((answer, index) => (
+          <>
+            <AnswerData answer={answer} />
+            {index < question.answers.length - 1 ? <hr /> : ""}
+          </>
+        ))
+      ) : (
+        <p>No answers</p>
+      )}
+      <SaveQuestionButton question={question} />
     </div>
   );
 };
