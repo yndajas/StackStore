603656ed45c44c07f38842b8122f4bcfc4a66bc9
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-18 20:48:25 +0100

    restructure answer data component

--- src/components/AnswerData.js
+++ src/components/AnswerData.js
@@ -1,24 +1,41 @@
-import React from "react";
+import { formatDate } from "../helpers/formatDate";
 
 const AnswerData = ({ answer }) => {
   return (
     <div>
-      <p>{answer.body}</p>
-      <h4>Stack meta</h4>
-      <a
-        href={`https://stackoverflow.com/a/${answer.stack_id}`}
-        title="View answer on Stack Overflow"
-      >
-        View answer on Stack Overflow
-      </a>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: answer.body,
+        }}
+      />
+      <p>----</p>
       <p>
-        Created: {answer.stack_created}
-        <br />
-        Updated: {answer.stack_updated}
-        <br />
+        {answer.accepted ? (
+          <>
+            <b>Accepted</b>
+            <br />
+          </>
+        ) : (
+          ""
+        )}
         Score: {answer.score}
         <br />
-        Accepted: {answer.accepted ? "yes" : "no"}
+        {answer.stack_updated ? (
+          <>
+            Updated: {formatDate(answer.stack_updated, "stack")}
+            <br />
+          </>
+        ) : (
+          ""
+        )}
+        Created: {formatDate(answer.stack_created, "stack")}
+        <br />
+        <a
+          href={`https://stackoverflow.com/a/${answer.stack_id}`}
+          title="View answer on Stack Overflow"
+        >
+          View answer on Stack Overflow
+        </a>
       </p>
     </div>
   );
