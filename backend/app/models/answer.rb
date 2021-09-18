52a18b5dcac95cde0cc4cb4b9a4c8e638e9f70e4
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-19 00:17:22 +0100

    fix hash references; deal with null data; transform dates

--- backend/app/models/answer.rb
+++ backend/app/models/answer.rb
@@ -8,11 +8,14 @@ class Answer < ApplicationRecord
   validates :body, length: { minimum: 1 }, presence: true
 
   def self.create_or_update_from_hash_and_question(hash, question)
-    answer = find_or_initialize_by(user_id: question.user_id, stack_id: hash[:stack_id])
+    answer = find_or_initialize_by(user_id: question.user_id, stack_id: hash[:id])
     answer.question = question
-    %w[stack_question_id accepted score body stack_created stack_updated notes].each do |attribute|
+    %w[stack_question_id accepted score body].each do |attribute|
       answer.send("#{attribute}=", hash[:"#{attribute}"])
     end
-    save
+    %w[stack_created stack_updated].each do |attribute|
+      answer.send("#{attribute}=", Time.at(hash[:"#{attribute}"])) if hash[:"#{attribute}"]
+    end
+    answer.save
   end
 end
