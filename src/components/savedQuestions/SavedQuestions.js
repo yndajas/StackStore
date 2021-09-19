232e07bce3eeb89ee22bea99d35796b27997021b
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-19 07:13:53 +0100

    rekey question data for saved questions

--- src/components/savedQuestions/SavedQuestions.js
+++ src/components/savedQuestions/SavedQuestions.js
@@ -16,13 +16,58 @@ class SavedQuestions extends React.Component {
   }
 
   render() {
+    const rekeyedAnswerData = (answerData) => {
+      return {
+        id: answerData.id,
+        ...answerData.attributes,
+        stack_created: Date.parse(answerData.attributes.stack_created),
+        stack_updated: Date.parse(answerData.attributes.stack_updated),
+      };
+    };
+
+    const rekeyedTagData = (questionTagData) => {
+      return { id: questionTagData.id, ...questionTagData.attributes };
+    };
+
+    const rekeyedQuestionData = (question) => {
+      const included = this.props.questions.included.filter(
+        (includedData) =>
+          includedData.attributes.stack_question_id ===
+          question.attributes.stack_id
+      );
+
+      const answers = included
+        .filter((includedData) => includedData.type === "answer")
+        .map((answer) => rekeyedAnswerData(answer));
+
+      const tags = included
+        .filter((includedData) => includedData.type === "question_tag")
+        .map((question_tag) => rekeyedTagData(question_tag));
+
+      const questionData = {
+        id: question.id,
+        ...question.attributes,
+        stack_created: Date.parse(question.attributes.stack_created),
+        stack_updated: Date.parse(question.attributes.stack_updated),
+        answers: answers,
+        tags: tags,
+      };
+
+      delete questionData.user_id;
+
+      return questionData;
+    };
+
     return (
       <div>
         <h2>Saved questions</h2>
-        {this.props.questions && this.props.questions.length > 0 ? (
+        {this.props.questions && this.props.questions.data.length > 0 ? (
           <div id="accordion">
             {this.props.questions.data.map((question) => (
-              <QuestionCard question={question} type={"saved"} />
+              <QuestionCard
+                question={rekeyedQuestionData(question)}
+                type={"saved"}
+              />
             ))}
           </div>
         ) : (
