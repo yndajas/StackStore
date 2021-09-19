d6ed22fad59d957c2386491f4e72f9d1a5c4c605
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-19 07:08:52 +0100

    remove unnecessary user ID rom answer serializer

--- backend/app/serializers/answer_serializer.rb
+++ backend/app/serializers/answer_serializer.rb
@@ -1,4 +1,4 @@
 class AnswerSerializer
   include JSONAPI::Serializer
-  attributes :user_id, :stack_id, :stack_question_id, :accepted, :score, :body, :stack_created, :stack_updated, :notes
+  attributes :stack_id, :stack_question_id, :accepted, :score, :body, :stack_created, :stack_updated, :notes
 end
