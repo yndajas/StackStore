7d5e1bcbbc40472b52f4fadaaefb1956250c6554
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-19 07:12:26 +0100

    add new tag container for questions route to differentiate from tags route

---
+++ src/containers/main/QuestionTagsContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class QuestionTagsContainer extends React.Component {
+  render() {
+    return <div>Question tags container</div>;
+  }
+}
+
+export default QuestionTagsContainer;
