c2a4172b5fd24385be19b029516e743e3282db5c
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-20 07:01:37 +0100

    add placeholder edit tags input component

---
+++ src/components/main/editQuestion/EditTagsInput.js
@@ -0,0 +1,18 @@
+// instructions (comma-separated)
+// text input
+// save button
+
+// send post request to /users/:user_id/questions/:question_id/tags
+// need to change backend to deal with processing multiple tags in one request
+// and deleting any removed tags at the same time (destroy action no longer needed)
+// then return the question rather than the tags?
+
+import React from "react";
+
+class EditTagsInput extends React.Component {
+  render() {
+    return <div>Edit tags input</div>;
+  }
+}
+
+export default EditTagsInput;
