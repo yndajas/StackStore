d239a0038baabcf068ece50e879da33162463b0e
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-20 20:44:36 +0100

    delete question tag routes (everything is now updated via questions)

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,9 +6,5 @@ Rails.application.routes.draw do
   scope 'users/:user_id' do
     resources :questions, except: %i[new edit], param: :question_id
     resources :tags, only: %i[index show], param: :tag_slug
-
-    scope 'questions/:question_id' do
-      resources :tags, only: %i[create destroy], controller: 'question_tags', param: :question_tag_id
-    end
   end
 end
