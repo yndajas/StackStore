16fdf4f9c4f85a0ff934b81829f9587b7b6608ad
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-20 07:01:23 +0100

    add placeholder delete question button component

---
+++ src/components/main/editQuestion/DeleteQuestionButton.js
@@ -0,0 +1,14 @@
+// button
+
+// send delete request with question ID
+// redirect to questions
+
+import React from "react";
+
+class DeleteQuestionButton extends React.Component {
+  render() {
+    return <div>Delete question button</div>;
+  }
+}
+
+export default DeleteQuestionButton;
