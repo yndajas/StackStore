03d607d5de5fc9ec993dba81ce63320e30bc42c4
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-20 06:06:49 +0100

    update component references

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 import "./App.scss";
-import Header from "./components/Header";
-import MainContainer from "./containers/MainContainer";
-import Footer from "./components/Footer";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <MainContainer />
+        <Main />
         <Footer />
       </div>
     );
