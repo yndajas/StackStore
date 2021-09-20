591c9553eb92eb3a8110a60edea49bd985ce39d4
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-20 06:52:39 +0100

    move log out button component

--- src/components/header/LogOutButton.js
+++ src/components/header/LogOutButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { logOut } from "../actions/logOut";
+import { logOut } from "../../actions/logOut";
 
 class LogOutButton extends React.Component {
   render() {
