e4cc63e767c7ec8bc82852c65a90844647fb9076
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-20 20:45:16 +0100

    change method tag_text to text; add custom creator using tag text string

--- backend/app/models/question_tag.rb
+++ backend/app/models/question_tag.rb
@@ -5,7 +5,16 @@ class QuestionTag < ApplicationRecord
   validates :question_id, uniqueness: { scope: :tag_id }, length: { minimum: 1 }, presence: true
   validates :tag_id, length: { minimum: 1 }, presence: true
 
-  def tag_text
+  def text
     tag.text
+  end
+
+  def self.create_or_update_from_texts_and_question(texts, question)
+    texts.each do |text|
+      unless question.tags.find_by(text: text)
+        tag = Tag.find_or_create_by(text: text)
+        create(question: question, tag: tag)
+      end
+    end
   end
 end
