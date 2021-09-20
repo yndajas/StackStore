20e77d3d94e2cfe494610ff6f6653def67883144
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-20 20:45:50 +0100

    add spacing

--- backend/app/models/answer.rb
+++ backend/app/models/answer.rb
@@ -10,12 +10,15 @@ class Answer < ApplicationRecord
   def self.create_or_update_from_hash_and_question(hash, question)
     answer = find_or_initialize_by(user_id: question.user_id, stack_id: hash[:stack_id])
     answer.question = question
+
     %w[stack_question_id accepted score body notes].each do |attribute|
       answer.send("#{attribute}=", hash[:"#{attribute}"]) if hash[:"#{attribute}"]
     end
+
     %w[stack_created stack_updated].each do |attribute|
       answer.send("#{attribute}=", Time.at(hash[:"#{attribute}"] / 1000)) if hash[:"#{attribute}"]
     end
+
     answer.save
   end
 end
