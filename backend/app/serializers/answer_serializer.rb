e5685cba4cd0e40206f32d1d7f4f81bc60328c35
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-20 05:59:47 +0100

    include question_id in answer serializer for matching to questions

--- backend/app/serializers/answer_serializer.rb
+++ backend/app/serializers/answer_serializer.rb
@@ -1,4 +1,5 @@
 class AnswerSerializer
   include JSONAPI::Serializer
-  attributes :stack_id, :stack_question_id, :accepted, :score, :body, :stack_created, :stack_updated, :notes
+  attributes :question_id, :stack_id, :stack_question_id, :accepted, :score, :body, :stack_created, :stack_updated,
+             :notes
 end
