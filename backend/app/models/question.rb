0a32f8b05dc9c7f8f5f836e707edee086f26cd19
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-20 20:45:44 +0100

    add tag creation and deletion to question update method

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -13,22 +13,31 @@ class Question < ApplicationRecord
     %w[score title body notes].each do |attribute|
       send("#{attribute}=", params[:"#{attribute}"]) if params[:"#{attribute}"]
     end
+
     %w[stack_created stack_updated].each do |attribute|
       send("#{attribute}=", Time.at(params[:"#{attribute}"] / 1000)) if params[:"#{attribute}"]
     end
+
     save
 
-    # add/update answers
     if params[:answers]
+      # add/update answers
       params[:answers].each do |answer_hash|
         Answer.create_or_update_from_hash_and_question(answer_hash, self)
       end
-    end
 
-    # remove answers no longer present in source
-    if params[:answers]
+      # remove answers no longer present in source
       answers_stack_ids = params[:answers].collect { |answer| answer[:stack_id] }
       answers.each { |answer| answer.destroy unless answers_stack_ids.include?(answer.stack_id) }
+    end
+
+    if params[:tags]
+      # add/update tags
+      texts = params[:tags].split(',').collect { |text| text.strip.downcase }
+      QuestionTag.create_or_update_from_texts_and_question(texts, self)
+
+      # delete tags no longer present in source
+      question_tags.each { |question_tag| question_tag.destroy unless texts.include?(question_tag.text) }
     end
   end
 end
