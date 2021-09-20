062d14ff1d30268003508f22a626ba9448bb440a
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-20 06:54:23 +0100

    move and complete saved question card body component

---
+++ src/components/main/savedQuestions/SavedQuestionCardBody.js
@@ -0,0 +1,30 @@
+import NotesParagraph from "./NotesParagraph";
+import TagsParagraph from "./TagsParagraph";
+import EditQuestionButton from "./EditQuestionButton";
+import QuestionData from "../QuestionData";
+import AnswerData from "../AnswerData";
+
+const SavedQuestionCardBody = ({ question }) => {
+  return (
+    <div className="card-body">
+      <NotesParagraph question={question} />
+      <TagsParagraph question={question} />
+      <EditQuestionButton question={question} />
+      <hr />
+      <h3>Question</h3>
+      <QuestionData question={question} />
+      <hr />
+      <h3>Answers</h3>
+      {question.answers && question.answers.length > 0
+        ? question.answers.map((answer, index) => (
+            <>
+              <AnswerData answer={answer} />
+              {index < question.answers.length - 1 ? <hr /> : ""}
+            </>
+          ))
+        : "No answers"}
+    </div>
+  );
+};
+
+export default SavedQuestionCardBody;
