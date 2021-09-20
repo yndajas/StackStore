ade4336cf75ff30098c89a592f9f1a0b790de266
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-20 21:00:33 +0100

    sort tags in question sorting helper

--- src/helpers/sortQuestions.js
+++ src/helpers/sortQuestions.js
@@ -1,8 +1,9 @@
 import { sortAnswers } from "./sortAnswers";
+import { sortTags } from "./sortTags";
 
 export const sortQuestions = (questions) => {
   return questions
     .sort((a, b) => parseInt(b.answer_count) - parseInt(a.answer_count))
     .sort((a, b) => parseInt(b.score) - parseInt(a.score))
-    .map((sortedItem) => sortAnswers(sortedItem));
+    .map((sortedQuestion) => sortTags(sortAnswers(sortedQuestion)));
 };
