3d7844b2ebd6a3bbcc275e01573efe5ff3188f92
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-21 06:09:22 +0100

    abstract accordion to component; update pre-load text

--- src/components/main/savedQuestions/SavedQuestions.js
+++ src/components/main/savedQuestions/SavedQuestions.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 import { fetchSavedQuestions } from "../../../actions/fetchSavedQuestions";
 import { clearQuestions } from "../../../actions/clearQuestions";
-import QuestionCard from "../QuestionCard";
+import SavedQuestionsAccordion from "./SavedQuestionsAccordion";
 
 class SavedQuestions extends React.Component {
   componentDidMount() {
@@ -20,22 +20,9 @@ class SavedQuestions extends React.Component {
       <div>
         <h2>Saved questions</h2>
         {this.props.questions && this.props.questions.length > 0 ? (
-          <>
-            <div id="accordion">
-              {this.props.questions.map((question) => (
-                <QuestionCard question={question} type={"saved"} />
-              ))}
-            </div>
-            <br />
-            <p>
-              <i>
-                * add/edit notes and tags, update question and answer data or
-                delete the question
-              </i>
-            </p>
-          </>
+          <SavedQuestionsAccordion questions={this.props.questions} />
         ) : (
-          "No saved questions"
+          "Loading or no saved questions"
         )}
       </div>
     );
