d9cadde56bff62c020c661bfe25652dae2bd987f
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-21 06:05:51 +0100

    update helper functions

--- src/helpers/sortQuestions.js
+++ src/helpers/sortQuestions.js
@@ -1,9 +1,11 @@
-import { sortAnswers } from "./sortAnswers";
-import { sortTags } from "./sortTags";
+import { sortAnswersWithQuestion } from "./sortAnswers";
+import { sortTagsWithQuestion } from "./sortTags";
 
 export const sortQuestions = (questions) => {
   return questions
     .sort((a, b) => parseInt(b.answer_count) - parseInt(a.answer_count))
     .sort((a, b) => parseInt(b.score) - parseInt(a.score))
-    .map((sortedQuestion) => sortTags(sortAnswers(sortedQuestion)));
+    .map((sortedQuestion) =>
+      sortTagsWithQuestion(sortAnswersWithQuestion(sortedQuestion))
+    );
 };
