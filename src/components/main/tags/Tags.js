d8b4dcef9e5a6b4b19b152afe22dc10cd24e36a3
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-21 06:07:16 +0100

    flesh out tags component

--- src/components/main/tags/Tags.js
+++ src/components/main/tags/Tags.js
@@ -1,17 +1,49 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { fetchSavedTags } from "../../../actions/fetchSavedTags";
+import { clearTags } from "../../../actions/clearTags";
+import TagLinkButton from "./TagLinkButton";
 
 class Tags extends React.Component {
   componentDidMount() {
     document.title = "Tags - StackStore";
+    this.props.fetchSavedTags(this.props.user);
+  }
+
+  componentWillUnmount() {
+    this.props.clearTags();
   }
 
   render() {
     return (
       <div>
-        <h2>Tags</h2>Tags container
+        <h2>Tags</h2>
+        {this.props.tags ? (
+          <>
+            <p>View questions by tag</p>
+            <div id="tag-link-buttons">
+              {this.props.tags.map((tag, index) => (
+                <>
+                  <TagLinkButton tag={tag} />
+                  {index < this.props.tags.length - 1 ? <>&nbsp;</> : ""}
+                </>
+              ))}
+            </div>
+          </>
+        ) : (
+          "No saved tags"
+        )}
       </div>
     );
   }
 }
 
-export default Tags;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    tags: state.tags,
+  };
+};
+
+export default connect(mapStateToProps, { fetchSavedTags, clearTags })(Tags);
