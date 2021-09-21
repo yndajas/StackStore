31b89add37fe3816f0795d4cc767646659b3286b
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-21 21:20:36 +0100

    update YouTube demo embed; add image credits; add link titles

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -18,12 +18,12 @@ class About extends React.Component {
           <li>add tags and notes to questions and view questions by tag</li>
         </ul>
         <hr />
-        <h3>Demo (UPDATE URL)</h3>
+        <h3>Demo</h3>
         <div class="embed-responsive embed-responsive-16by9 iframe_container">
           <iframe
             title="YouTube demo"
             class="embed-responsive-item"
-            src="https://www.youtube.com/embed/PYwX1QGj6os"
+            src="https://www.youtube.com/embed/47qTNiLzgQU"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen="allowfullscreen"
@@ -33,14 +33,70 @@ class About extends React.Component {
         <h3>Creator and GitHub repository</h3>
         <ul>
           <li>
-            Ynda Jas (<a href="https://pronoun.is/they/.../themself">they</a>):{" "}
-            <a href="https://github.com/yndajas">GitHub</a> |{" "}
-            <a href="https://yndajas.co">website</a>
+            Ynda Jas (
+            <a
+              href="https://pronoun.is/they/.../themself"
+              title="Creator pronouns"
+            >
+              they
+            </a>
+            ):{" "}
+            <a href="https://github.com/yndajas" title="Creator GitHub profile">
+              GitHub
+            </a>{" "}
+            |{" "}
+            <a href="https://yndajas.co" title="Creator website">
+              website
+            </a>
           </li>
           <li>
-            <a href="https://github.com/yndajas/StackStore">
+            <a
+              href="https://github.com/yndajas/StackStore"
+              title="GitHub repository"
+            >
               GitHub repository
             </a>
+          </li>
+        </ul>
+        <h3>Image credits</h3>
+        <ul>
+          <li>
+            Logo icon/favicon based on shape (colour edited for use on
+            StackStore) by{" "}
+            <a
+              href="https://www.flaticon.com/authors/freepik"
+              title="Freepik on Flaticon"
+            >
+              Freepik
+            </a>
+            , available{" "}
+            <a
+              href="https://www.flaticon.com/free-icon/stacked-files_17640"
+              title="Logo icon/favicon"
+            >
+              here
+            </a>
+          </li>
+          <li>
+            Background image by{" "}
+            <a href="https://www.sanderottens.com" title="Sander Ottens">
+              Sander Ottens
+            </a>
+            , available at{" "}
+            <a
+              href="https://www.transparenttextures.com"
+              title="Background image source one"
+            >
+              Transparent Textures
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://www.toptal.com/designers/subtlepatterns/cubes"
+              title="Background image source two"
+            >
+              Subtle Patterns
+            </a>{" "}
+            (by Toptal)
           </li>
         </ul>
       </div>
