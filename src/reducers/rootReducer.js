d4d9a71e4627eee89f62183391948cb4556795c6
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-21 06:11:16 +0100

    add tag-related actions; add comments to indicate reducer sections

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 export default function rootReducer(state = {}, action) {
   switch (action.type) {
+    // sessions
     case "LOG_IN":
       return { ...state, user: action.payload };
     case "LOG_OUT":
       return {};
+    // search
     case "SET_SEARCH_QUERY":
       return { ...state, searchQuery: action.payload };
     case "SEARCH_QUESTIONS":
@@ -28,14 +30,15 @@ export default function rootReducer(state = {}, action) {
       } else {
         return { ...state, savedQuestions: [action.payload] };
       }
+    // saved questions
     case "FETCH_SAVED_QUESTIONS":
       return { ...state, questions: action.payload };
     case "CLEAR_QUESTIONS":
       const newStateCQs = { ...state };
       delete newStateCQs.questions;
       return newStateCQs;
-    case "FETCH_SAVED_QUESTION":
-    // fall through
+    // saved question
+    case "FETCH_SAVED_QUESTION": // fall through
     case "UPDATE_QUESTION":
       return { ...state, question: action.payload };
     case "DELETE_QUESTION":
@@ -44,6 +47,20 @@ export default function rootReducer(state = {}, action) {
       const newStateCQ = { ...state };
       delete newStateCQ.question;
       return newStateCQ;
+    // saved tags
+    case "FETCH_SAVED_TAGS":
+      return { ...state, tags: action.payload };
+    case "CLEAR_TAGS":
+      const newStateCTs = { ...state };
+      delete newStateCTs.tags;
+      return newStateCTs;
+    // saved questions by tag
+    case "SET_TAG":
+      return { ...state, tag: action.payload };
+    case "CLEAR_TAG":
+      const newStateCT = { ...state };
+      delete newStateCT.tag;
+      return newStateCT;
     default:
       return state;
   }
