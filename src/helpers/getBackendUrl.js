b91405bc3505a87472b3d5b3bc966c7e16e29ecd
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-21 06:50:31 +0100

    add production backend URL

--- src/helpers/getBackendUrl.js
+++ src/helpers/getBackendUrl.js
@@ -2,9 +2,8 @@ export const getBackendUrl = () => {
   switch (process.env.NODE_ENV) {
     case "development":
       return "http://localhost:3000";
-    case "production":
-    // fall through
+    case "production": // fall through
     default:
-      return "TBC";
+      return "https://stackstore-backend.herokuapp.com";
   }
 };
