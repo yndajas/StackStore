de947f6e6f48021eb0374a02e89352620a2e4c63
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-09-21 06:00:55 +0100

    move serialized question JSON helper to application controller for reuse

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::API
   def render_error_if_record_not_found(record)
     render json: { error: 'Record not found' }, status: 404 unless record
   end
+
+  def serialized_question_json(questions)
+    QuestionSerializer.new(questions, include: %i[answers question_tags])
+  end
 end
